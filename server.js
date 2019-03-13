f9e4338bea416f0cd3ab190d750ef3dfbeed27f6
poornima.balagopal@gmail.com <poornima@14>
2019-03-13 14:44:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,12 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static(path.join(__dirname, '/app')));
 app.use('/', express.static(path.join(__dirname, '/')))
 
